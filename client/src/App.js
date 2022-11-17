e01e0b67ff684f828ced1763ee0f9eae793d08da
Lisa Smeke <lisasmeke@gmail.com>
2022-11-17 16:24:16 +0100

    Add label, input & btn to create list

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,43 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [list, setList] = useState('');
+  const [todo, setTodo] = useState('');
+
+  const displayInfo = () => {
+    console.log(list + todo);
+  };
+
+  return (
+    <div className="App">
+      <div className="signIn">
+        <label>Username:</label>
+        <input type="text" />
+        <label>Password:</label>
+        <input type="password" />
+        <button>Sign in</button>
+      </div>
+      <div className="createList">
+        <h2>My To-do Lists</h2>
+        <label>Title:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setList(event.target.value);
+          }}
+        />
+        <label>to-do:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setTodo(event.target.value);
+          }}
+        />
+        <button onClick={displayInfo}>Create list</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
